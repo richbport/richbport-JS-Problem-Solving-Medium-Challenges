842d06f44521d565fcb8e9508105d2fa66363437
Rich <richbport@yahoo.com>
2025-05-17 16:16:22 -0400

    Q1 additional

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 function filterOutFalsy(elem1, elem2) {
-    if (true) {
+    if (!true) {
         // return the element
             return elem1;
     }
     else {
+        console.log('this ran')
         // return element 2
             return elem2;
     }
 }
+
+filterOutFalsy(0, 500)
