3ff5568aff9d0a6d42a590d670af98f081f55fc0
Rich <richbport@yahoo.com>
2025-05-17 17:16:58 -0400

    Q1 ternary operator

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 function filterOutFalsy(elem1, elem2) {
-    if (!true) {
-        // return the element
-            return elem1;
-    }
-    else {
-        console.log('this ran')
-        // return element 2
-            return elem2;
-    }
+    return !elem1 ? elem1 : elem2;
 }
 
-filterOutFalsy(0, 500)
+console.log(filterOutFalsy(0, 5))
