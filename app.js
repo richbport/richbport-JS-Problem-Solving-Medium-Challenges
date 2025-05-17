0ce73acf30f3fb646c0f2463904b6fa52a8573d9
Rich <richbport@yahoo.com>
2025-05-17 15:35:55 -0400

    Q1

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 function filterOutFalsy(elem1, elem2) {
-    if (/*falsy*/) {
+    if (true) {
         // return the element
+            return elem1;
     }
     else {
         // return element 2
+            return elem2;
     }
 }
