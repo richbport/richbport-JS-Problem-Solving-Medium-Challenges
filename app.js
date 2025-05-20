36bf12b598d96dfcdfd3e8bd3dbd6d141c6729a8
Rich <richbport@yahoo.com>
2025-05-19 20:16:15 -0400

    Q4 continued

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-function arrSum(arr) {
-  for (let i = 0; i < 3; ++i) {
-    console.log("run this code");
+  function arrSum(arr) {
+    for (let i = 0; i < arr.length; ++i) {
+    console.log(arr[i]);
   }
-}
+  console.log('for loop broke')
+  }
 
-console.log(arrSum([2, 2, 2]));
+console.log(arrSum([1, 2, 3, 4, 5, 6]));
