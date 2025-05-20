4cb430b6f583dbb0e32b59c56669e10661d7c238
Rich <richbport@yahoo.com>
2025-05-20 12:54:16 -0400

    Q5 i = 1; i <= i

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 function progressiveSum(num) {
   let sum = 0;
-  for (let i = 0; i < num; ++i) {
-    console.log(i)
+  for (let i = 1; i <= num; ++i) {
+    sum = sum + i;
   }
+  return sum;
 }
 
-console.log(progressiveSum(3));
+console.log(progressiveSum(4));
