11f18becfc73086c488ede66f2fa502ab64db0d4
Rich <richbport@yahoo.com>
2025-05-20 10:10:48 -0400

    Q4 arr.length - 1

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function arrSum(arr) {
   let sum = 0;
-  for (let i = 0; i < arr.length; ++i) {
+  for (let i = 0; i < arr.length - 1; ++i) {
     sum = sum + arr[i];
   }
   return sum;
 }
 
-console.log(arrSum([1, 2, 3, 4, 5, 6]));
+console.log(arrSum([2, 2, 2]));
