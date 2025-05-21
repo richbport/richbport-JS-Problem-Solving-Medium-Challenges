9699231331dd1a5d896f2dca3d54c466d68f66fe
Rich <richbport@yahoo.com>
2025-05-21 14:00:39 -0400

    Q6 timerMinutes and timerSeconds

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 function calcTime(seconds) {
   let timerMinutes = Math.floor(seconds / 60);
   let timerSeconds = seconds % 60;
+
+return '0' + timerMinutes + ':' + timerSeconds;
 }
 
-console.log(calcTime(50));
+console.log(calcTime(50000));
