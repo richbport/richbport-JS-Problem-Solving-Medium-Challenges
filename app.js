db0066ec424269d14c684de5e103cc3dfbf522f5
Rich <richbport@yahoo.com>
2025-05-21 23:18:45 -0400

    Q7 loop through the array

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-function getMax(num) {
-  for (let i = 0; i < length - 1; ++i) 
+function getMax(arr) {
+  for (let i = 0; i < arr.length; ++i) {
+    console.log(arr[i]);
+  }
 }
 
 console.log(getMax([-300, -100, -200]));
