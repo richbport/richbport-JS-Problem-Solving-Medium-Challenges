a7f4b8aae9f697cb24d187970e0076c5fc055935
Rich <richbport@yahoo.com>
2025-05-23 16:25:06 -0400

    Q8 array reverse property

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 function reverseString(str) {
-  let reversedString = "";
-  for (let i = str.length - 1; i >= 0; --i) {
-    reversedString += str[i];
-  }
-  return reversedString;
+  return str.split('').reverse().join('');
 }
 
-console.log(reverseString("abc"));
+console.log(reverseString("David"));
