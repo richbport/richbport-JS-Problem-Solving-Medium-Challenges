7f04758fd15d60752d4fe62b5a94baecf7617a72
Rich <richbport@yahoo.com>
2025-05-22 21:30:22 -0400

    Q7 altered

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ function getMax(arr) {
   let max = arr[0];
   for (let i = 1; i < arr.length; ++i) {
     if (arr[i] > max) {
-      max = arr[i];
-    }
+    max = arr[i];
   }
-  return max;
+}
+return max
 }
 
-console.log(getMax([1, 3, 5]));
+console.log(getMax([1, 3, 5]))
