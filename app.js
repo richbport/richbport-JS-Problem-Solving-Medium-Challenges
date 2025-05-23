8eca9f7f055c196e55a5ccc5d51f508c53eac7ea
Rich <richbport@yahoo.com>
2025-05-23 19:43:49 -0400

    Q9 return an object in arr.map

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function convertToZeros(arr) {
- return arr.map((elem) => 0);
+  return arr.map((elem) => {
+    return 0;
+  });
 }
 
-console.log(convertToZeros([1, 2, 3]));
+console.log(convertToZeros([1, 2, 3, 4, 5]));
