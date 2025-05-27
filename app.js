8e067a364522028e65243bae21e7407767562a7e
Rich <richbport@yahoo.com>
2025-05-27 19:35:11 -0400

    Q12 altered

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function convertToBoolean(arr) {
-    return arr.map(elem => !!elem)
+  return arr.map((elem) => !!elem);
 }
 
-console.log(convertToBoolean([500, 0, "David", "", []]));
+console.log(convertToBoolean([500, 0, NaN, "David", "", []]));
