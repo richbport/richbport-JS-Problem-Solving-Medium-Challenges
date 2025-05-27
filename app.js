861b669e06c8e1ae32e1a54169e2fcf1b6f077f3
Rich <richbport@yahoo.com>
2025-05-27 18:16:57 -0400

    Q11 arr.filter variation

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 function filterOutFalsy(arr) {
-    let truthyArr = []
-    for (let i = 0; i < arr.length; ++i) {
-        if (!!arr[i] === true) {
-            truthyArr.push(arr[i])
-        }
-    }
-  return truthyArr;
+  return arr.filter(elem => !!elem);
 }
 
-console.log(filterOutFalsy(["", NaN, 500, undefined, null, false, 0]));
+console.log(filterOutFalsy(["", NaN, 500, undefined, null, false, 0, "0"]));
