ab7df1f7ccbf06ffb4831a39659c776b446b4d5c
Rich <richbport@yahoo.com>
2025-05-28 23:03:08 -0400

    Q1 another way

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function showRating(rating) {
-let ratings = ""
-for (let i = 0; i < 4; ++i) {
-    if (i === 4) {
-        ratings = ratings + "*"
+  let ratings = "";
+  for (let i = 0; i < 4; ++i) {
+    ratings = ratings + "*";
+    if (i !== 3) {
+      ratings = ratings + " ";
     }
-    ratings = ratings + "* "
-}
-return ratings;
+  }
+  return ratings;
 }
 
 console.log(showRating(4));
