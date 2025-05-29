edc4930dafee2901d7ed464bb38d439ac0efaf65
Rich <richbport@yahoo.com>
2025-05-28 22:20:14 -0400

    Q1 altered

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 function showRating(rating) {
-// return rating
-console.log(rating)
+let ratings = ""
+for (let i = 0; i < 4; ++i) {
+    if (i === 4) {
+        ratings = ratings + "*"
+    }
+    ratings = ratings + "* "
+}
+return ratings;
 }
 
 console.log(showRating(4));
